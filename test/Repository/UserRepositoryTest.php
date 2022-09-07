9442cf8e0402d620acfdf6de6910ab72d8df9536
yusharnadi <yust.44@gmail.com>
2022-09-07 22:35:42 +0700

    add user repo test

---
+++ test/Repository/UserRepositoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Repository;
+
+use PHPUnit\Framework\TestCase;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
+
+class UserRepositoryTest extends TestCase
+{
+
+    private UserRepository $userRepository;
+
+    public function setUp(): void
+    {
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->userRepository->deleteAll();
+    }
+
+    public function testSaveSuccess()
+    {
+        $user = new User();
+        $user->id = "eko";
+        $user->name = "Eko";
+        $user->password = "password";
+
+        $this->userRepository->save($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+
+        $this->assertEquals($user->id, $result->id);
+        $this->assertEquals($user->name, $result->name);
+        $this->assertEquals($user->password, $result->password);
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $user = $this->userRepository->findById('notfound');
+
+        $this->assertNull($user);
+    }
+}
