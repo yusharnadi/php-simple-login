0179b8ae953b02ad01d187f70c2002128c69563c
yusharnadi <yust.44@gmail.com>
2022-09-07 22:34:41 +0700

    add view template

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -16,37 +16,4 @@ class HomeController
 
         View::render('Home/index', $model);
     }
-
-    function hello(): void
-    {
-        echo "HomeController.hello()";
-    }
-
-    function world(): void
-    {
-        echo "HomeController.world()";
-    }
-
-    function about(): void
-    {
-        echo "Author : Eko Kurniawan Khannedy";
-    }
-
-    function login(): void
-    {
-        $request = [
-            "username" => $_POST['username'],
-            "password" => $_POST['password']
-        ];
-
-        $user = [
-
-        ];
-
-        $response = [
-            "message" => "Login Sukses"
-        ];
-        // kirimkan response ke view
-    }
-
 }
