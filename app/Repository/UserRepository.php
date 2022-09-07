368597d33a2ce907277d50370d12acf5ff71e20e
yusharnadi <yust.44@gmail.com>
2022-09-07 22:35:20 +0700

    add user repo

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Repository;
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
+
+class UserRepository
+{
+    private \PDO $connection;
+
+    public function __construct(\Pdo $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function save(User $user): User
+    {
+        $stmt = $this->connection->prepare("INSERT INTO users (id, name, password) VALUES(?,?,?)");
+        $stmt->execute(
+            [
+                $user->id, $user->name, $user->password
+            ]
+        );
+
+        return $user;
+    }
+
+    public function findById(string $id): ?User
+    {
+        $stmt = $this->connection->prepare("SELECT id,name,password FROM users WHERE id=?");
+        $stmt->execute([$id]);
+
+        try {
+
+            if ($row = $stmt->fetch()) {
+                $user = new User();
+                $user->id = $row['id'];
+                $user->name = $row['name'];
+                $user->password = $row['password'];
+
+                return $user;
+            } else {
+                return null;
+            }
+        } finally {
+            $stmt->closeCursor();
+        }
+    }
+
+    public function deleteAll(): void
+    {
+        $this->connection->exec("DELETE FROM users");
+    }
+}
